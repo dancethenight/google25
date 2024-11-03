eee78ff004185e4d8b9b8fb147c87190a353096c
Ash V <ashutoshv191@gmail.com>
2024-11-03 22:35:14 +0530

    0

--- math.js
+++ math.js
@@ -1,7 +1,26 @@
-Math.gcd = (a, b) => { // of 2 no.s how for an array?
-    // check type somehow
-    if (b === 0) {
-        return a
+Math.swap = (a, i, j) => [a[i], a[j]] = [a[j], a[i]]
+
+Math.gcd = (...a) => {
+    const _gcd = (x, y) => {
+        if (y === 0) return x
+        return _gcd(y, x % y)
     }
-    return Math.gcd(b, a % b)
+
+    return a.reduce((x, y) => _gcd(x, y))
+}
+
+Math.arrayLowerBound = (a, x) => {
+    let lo = 0,
+        hi = a.length - 1
+    while (lo + 1 < hi) {
+        let md = (lo + hi) >> 1
+        if (a.at(md) < x)
+            lo = md + 1
+        else
+            hi = md
+    }
+
+    if (a.at(lo) >= x) return lo
+    if (a.at(hi) >= x) return hi
+    return -1
 }
